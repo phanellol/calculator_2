bc749894429c6955a11f7f6f061466a9e0f3087c
Pavlo Phanel <devpavlophanel@gmail.com>
2025-06-15 20:45:11 +0200

    Completed step 4

--- dist/script.js
+++ dist/script.js
@@ -37,4 +37,17 @@ function operate() {
     }
 }
 const main = document.querySelector("#main");
-console.log(main);
+const input = document.createElement("input");
+input.id = "userInput";
+input.placeholder = "0";
+main === null || main === void 0 ? void 0 : main.appendChild(input);
+const buttonContainer = document.createElement("div");
+buttonContainer.classList.add("buttonContainer");
+main === null || main === void 0 ? void 0 : main.appendChild(buttonContainer);
+const valueArray = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "*", "/", "=", "c"];
+for (let i = 0; i < valueArray.length; i++) {
+    const numButton = document.createElement("button");
+    numButton.classList.add("numButton");
+    numButton.textContent = valueArray[i];
+    buttonContainer.appendChild(numButton);
+}
